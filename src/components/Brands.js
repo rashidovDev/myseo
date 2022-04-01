3c1dacb9cac6b2213668b8dc3342052ddda3a697
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-04-01 22:57:23 +0300

    added

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,22 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import brands data
 import { brands } from '../data';
 
-const Companies = () => {
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+const Brands = () => {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const { ref, inView } = useInView();
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      setIsAnimating(true);
+    } else {
+      setIsAnimating(false);
+    }
+  });
   return (
-    <section className='min-h-[146px] bg-tertiary flex items-center'>
-      <div className='container mx-auto flex md:justify-between items-center flex-wrap justify-evenly'>
+    <section ref={ref} className='min-h-[146px] bg-tertiary flex items-center'>
+      <motion.div
+        variants={container}
+        initial='hidden'
+        animate={isAnimating ? 'visible' : ''}
+        className='container mx-auto flex md:justify-between items-center flex-wrap justify-evenly'
+      >
         {brands.map((brand, idx) => {
           return (
-            <div key={idx}>
-              <img src={brand.img} alt='' />
+            <div
+              variants={container}
+              initial='hidden'
+              animate={isAnimating ? 'visible' : ''}
+              key={idx}
+            >
+              <motion.img variants={item} src={brand.img} alt='' />
             </div>
           );
         })}
-      </div>
+      </motion.div>
     </section>
   );
 };
 
-export default Companies;
+export default Brands;
