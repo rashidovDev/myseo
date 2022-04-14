0720deef5199cf7138a8a65f2d2d8b5b629efd7c
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-04-14 21:32:21 +0300

    added

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -10,8 +10,27 @@ import { navigation } from '../data';
 // import components
 import Socials from './Socials';
 
+// import framer
+import { motion } from 'framer-motion';
+
 const NavMobile = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const circleVariants = {
+    hidden: {
+      opacity: 0,
+      scale: 0,
+    },
+    visible: {
+      opacity: 1,
+      scale: 180,
+      transition: {
+        duration: 0.5,
+        type: 'tween',
+      },
+    },
+  };
+
   return (
     <nav className='relative'>
       <div
@@ -21,10 +40,18 @@ const NavMobile = () => {
         <MenuAlt3Icon className='w-8 h-8' />
       </div>
 
+      {/* circle */}
+      <motion.div
+        variants={circleVariants}
+        initial='hidden'
+        animate={isOpen ? 'visible' : 'hidden'}
+        className='w-4 h-4 rounded-full bg-accent fixed top-0 right-0 -z-10'
+      ></motion.div>
+
       <ul
         className={`${
           isOpen ? 'right-0' : '-right-full'
-        } fixed bg-accent top-0 bottom-0 w-full transition-all duration-300`}
+        } fixed top-0 bottom-0 w-full flex flex-col justify-center items-center transition-all duration-300 overflow-hidden`}
       >
         <div onClick={() => setIsOpen(false)} className='cursor-pointer'>
           <XIcon className='w-8 h-8' />
