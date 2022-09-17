441ceea7901e2f53db2a44cd0542a5a3affd5516
rashidovDev <anvarrashidov17@gmail.com>
2022-09-17 09:02:36 +0500

    fix href..

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -11,13 +11,13 @@ const Hero = () => {
       <div className='container mx-auto h-full '>
         <div className='flex items-center h-full align-center  pt-8'>
           <div className=' flex-1 flex flex-col items-center lg:items-start'>
-            <p className='text-4xl text-accent text-md mb-[22px] '>
+            <p className='text-4xl text-accent text-md mb-[22px]'>
               Hi there, I am Anvar
             </p>
-            <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px] mb-7 text-center'>
+            <h1 className='text-4xl leading-[44px] md:text-center md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px] mb-7 text-center'>
               I am Frontend <br />Developer 
             </h1>
-            <a href='http://t.me/anvar_rashidov' className='btn btn-md bg-accent hover:bg-secondary-hover md:btn-lg transition-all rounded-[12px] cursor-pointer'>
+            <a href='http://t.me/anvar_rashidov' className='btn btn-md lg:ml-32 bg-accent hover:bg-secondary-hover  md:btn-lg transition-all rounded-[12px] cursor-pointer'>
                Contact Me
             </a>
           </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Project = ({ item }) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
+      <a href={item.href}>
       <div className='mb-8'>
         <img className='rounded-2xl' src={item.image} alt='imag' width={250} height={300} />
       </div>
       <p className='capitalize text-accent text-sm mb-3'>{item.category}</p>
-      <a href={item.href} className='text-2xl text-slate-50 font-semibold capitalize mb-3'>{item.name}</a>
+      <p className='text-2xl text-slate-50 font-semibold capitalize mb-3'>{item.name}</p>
       <p className='text-base max-w-md'>
        {item.title}
       </p>
+      </a>
     </div>
   );
 };
