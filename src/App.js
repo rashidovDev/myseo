6cb100dc553c26262fb53d435678d22db26b5b83
rashidovDev <anvarrashidov17@gmail.com>
2022-09-22 18:44:00 +0500

    add loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import HashLoader from "react-spinners/HashLoader";
 // import components
 import Header from './components/Header';
 import Hero from './components/Introduction';
@@ -11,7 +11,22 @@ import Footer from './components/Footer';
 import BackTopBtn from './components/BackTopBtn';
 
 const App = () => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true) 
+      setTimeout(() => {
+         setLoading(false)
+      },4000)
+  },[])
   return (
+    <>
+    {
+      loading ?  
+      <div className='w-full h-screen bg-black flex justify-center items-center'>
+              <HashLoader className='' color={"#AC6B34"} loading={loading} size={60} />
+      </div>
+      :
     <div className='bg-black relative'>
       <Header />
       <Hero />
@@ -22,6 +37,9 @@ const App = () => {
       <Footer />
       <BackTopBtn />
     </div>
+    }
+     
+    </>
   );
 };
 
