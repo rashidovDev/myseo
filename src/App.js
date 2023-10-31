27b61727c145528671d33e65dca70c6cb2718e03
unknown <anvarrashidov17@gmail.com>
2023-11-01 03:43:08 +0900

    fixed problem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Hero from './components/Introduction';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
-import Services from './components/Services';
 import Footer from './components/Footer';
 import BackTopBtn from './components/BackTopBtn';
 
